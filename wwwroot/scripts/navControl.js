707263fe45271726114f949b3918934a7b7e87cc
DarrenG441 <darren.h.gill@googlemail.com>
2017-06-20 15:35:05 +0100

    Changed Knockout menu generation to use templates recursivley
    also implemented te dynamicPopup method

--- wwwroot/scripts/navControl.js
+++ wwwroot/scripts/navControl.js
@@ -7,25 +7,17 @@ NB: Pass the jQuery noConfilct into the $ parameter
 (function (navControl, $, undefined) {
     //Revealing Module Pattern
 
-    navControl.debugState = function () {
-        for (var p in this) {
-            if (typeof this[p] != "function") console.log("Value of " + p + " is " + this[p].toString());
-        }
-        console.log("isModalOpen: " + isModalOpen);
-        console.log("_openPanelName: " + _openPanelName);
-        console.log("_bookmarkName: " + _bookmarkName);
-    }
-
     var isModalOpen = false;
     var _openPanelName = null;
     var _bookmarkName = null;
     ///This template will be used to add content to the foot of a document and then displayed as a popup
-    var _templatePopup = '<div class="w3-container w3-modal w3-animate-zoom w3-padding-16" id="[[popupPanelID]]" ><div class="w3-modal-content "><div class="w3-content"><header class="[[popupPanelHeaderCSS]] w3-container"><div class="w3-right"><a href="javascript:void(0)" onclick="navControl.modal(\'[[popupPanelID]]\',true)"><i class="fa fa-window-close" aria-hidden="true"></i></a></div><span data-bind="text:$root.currentItem().Name">[[popupPanelName]]</span></header><div class="w3-panel"><div class="w3-content" id="[[popupPanelID]]_content">Loading Content to Display ...</div></div><header class="[[popupPanelHeaderCSS]] w3-container"><div class="w3-right"><a href="javascript:void(0)" onclick="navControl.modal(\'[[popupPanelID]]\',true)"><i class="fa fa-window-close" aria-hidden="true"></i></a></div><span data-bind="text:$root.currentItem().Name">[[popupPanelName]]</span></header></div></div></div>';
+    var _templatePopup = '<div class="w3-container w3-modal w3-animate-zoom" id="[[popupPanelID]]" ><div class="w3-modal-content"><div class="w3-content"><header class="[[popupPanelHeaderCSS]] w3-container"><div class="w3-right"><a href="javascript:void(0)" onclick="navControl.modal(\'[[popupPanelID]]\',true)"><i class="fa fa-window-close" aria-hidden="true"></i></a></div><span>[[popupPanelName]]</span></header><div class="w3-panel"><div class="w3-content" id="[[popupPanelID]]_content">Loading Content to Display ...</div></div><header class="[[popupPanelHeaderCSS]] w3-container"><div class="w3-right"><a href="javascript:void(0)" onclick="navControl.modal(\'[[popupPanelID]]\',true)"><i class="fa fa-window-close" aria-hidden="true"></i></a></div><span>[[popupPanelName]]</span></header></div></div></div>';
     var _defPopupOptions = {
         popupPanelID: null, //Expect this to be generated later if required
         popupPanelHeaderCSS: "w3-teal",
         popupPanelName: "" //expect this to come from a menu name working in popup mode
-    }
+    };
+    var _dynamicDivPanelCounter = 1000;
     
     var processHash = function (sBookmark) {
         //have a bookmark so check to see if it is valid
@@ -39,7 +31,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
         if (tagBookmark) {
             if (tagBookmark.innerHTML) {
                 //content already present, no special processing required as the browser should scrol into view ...
-                console.log("Standard Bookmark Processing. If you need to call any analyticts to register this 'view' then this could be the place....")
+                console.log("Standard Bookmark Processing. If you need to call any analyticts to register this 'view' then this could be the place....");
             } else {
                 //No content.try and load it ....
                 $.ajax(urlContent, {
@@ -50,7 +42,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
                     error: function (jqXHR, textStatus, errorThrown) {
                         console.log("Failure to load requested content from " + urlContent);
                     }
-                })
+                });
                 
             }
         } else {
@@ -62,7 +54,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
                     var jqBookmark = $("<a name='" + sBookmark + "'></a>");
                     tagBookmark = jqBookmark.get(0);
                     tagBookmark.innerHTML = data;
-                    tagBookmark
+                    
                     $("body").append(tagBookmark);
 
                     //ensure a bit of margin padding is added (assuming that the W3.CSS stylesheet in use!)
@@ -75,7 +67,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
                 error: function (jqXHR, textStatus, errorThrown) {
                     console.log("Failure to load requested content from " + urlContent);
                 }
-            })
+            });
         }
     };
 
@@ -102,8 +94,8 @@ NB: Pass the jQuery noConfilct into the $ parameter
       * @param { function } cbError function(errorText){}
       * @return {array<menuItem>}
     */
-    navControl.buildMenu = function (url, menuID, cbReady , cbError) {
-        
+    navControl.buildMenu = function (url, menuID, cbReady, cbError) {
+        console.log("buildMenu started");
         if (!url) throw "Source Url Required";
 
         if (!cbReady) throw "Expect to have a callback to receive the menu object array";
@@ -116,13 +108,18 @@ NB: Pass the jQuery noConfilct into the $ parameter
         var fMenuWalker = function (oContainer, tagContainer) {
             var aMenuItem = [];
             $(tagContainer).children().each(function (idx, tag) {
-                
+
                 if (tag.tagName === "menuitem") {
                     var newMenuItem = {//NB: Have to use the first filter to prevent descendant menuitem names and urls getting picked up
                         name: $("name", tag).first().text(),
                         url: $("url", tag).first().text(),
-                        target: $("target", tag).first().text()
+                        target: $("target", tag).first().text(),
+                        popup: $("popup", tag).length ? true : false //existance of a popup tag indicates that a modal popup should be used
                     };
+
+                    if (newMenuItem.popup) {
+                        newMenuItem.url = "JAVASCRIPT:navControl.dynamicPopup('" + newMenuItem.url + "','" + newMenuItem.name + "')";
+                    }
                     //recurse
                     fMenuWalker(newMenuItem, tag);
                     aMenuItem.push(newMenuItem);
@@ -134,7 +131,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
             }
             return;
 
-        }
+        };
         
         $.ajax({
             url: url,
@@ -144,7 +141,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
                 var sSelector = "menus > menu";
                 
                 if (menuID) {
-                    if (typeof menuID == "number") {
+                    if (typeof menuID === "number") {
                         sSelector += ":nth-child(" + menuID + ")"; //NB. 1 based idexing, which is fine because if a zero gets passed the  if(menuID) is falsey so will just get 1st menu anyway!
                     } else {
                         sSelector += "[id='" + menuID + "']"; //may need to check the content?
@@ -159,15 +156,16 @@ NB: Pass the jQuery noConfilct into the $ parameter
 
                     var retMenu = {};
                     fMenuWalker(retMenu, tagMenu); //start the recurse process
+                    console.log("buildMenu completed sucessfully. Invoking callback ...");
                     cbReady(retMenu);
                 } catch (ex) {
-                    var sErrorMessage = (ex.message || "No Message!")
+                    var sErrorMessage = ex.message || "No Message!";
                     console.error("navControl.buildMenu Error processing menu DOM\n" + sErrorMessage);
-                    if (cbError && typeof cbError == "function") cbError(sErrorMessage);
+                    if (cbError && typeof cbError === "function") cbError(sErrorMessage);
                     return;
                 }
             },
-            error: function (jqXHR, statusText, errorText) { console.error("navControl.buildMenu Error:" + errorText); if (cbError && typeof cbError == "function") cbError(errorText); return; }
+            error: function (jqXHR, statusText, errorText) { console.error("navControl.buildMenu Error:" + errorText); if (cbError && typeof cbError === "function") cbError(errorText); return; }
 
         });
 
@@ -175,7 +173,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
 
 
     //capture keyup to process an escape on a modal panel
-    $(document).keyup(function (e) { if (e.keyCode == 27 && _openPanelName) navControl.modal(_openPanelName, true); return; });
+    $(document).keyup(function (e) { if (e.keyCode === 27 && _openPanelName) navControl.modal(_openPanelName, true); return; });
 
     //Everybody loves a Lightbox modal display!
     // The W3.CSS has a helper class for this 
@@ -192,7 +190,7 @@ NB: Pass the jQuery noConfilct into the $ parameter
                     _openPanelName = panelName;
                 } else {
                     isModalOpen = false;
-                    _openPanelName = null
+                    _openPanelName = null;
                 }
             } else {
                 console.error("ID passed (" + panelName + ") is a valid emlement identifier, but the element does not have the CSS class w3-modal so will not be displayed!");
@@ -206,8 +204,49 @@ NB: Pass the jQuery noConfilct into the $ parameter
         return null;
     };
 
-    //Generate a new DOM element from 
-    navControl.dynamicPopup = function () {
+    //Generate a new DOM element from the template and load 
+    // with the content specified by the URL
+    navControl.dynamicPopup = function (url, options) {
+
+    /*  {
+             popupPanelID: null, //Expect this to be generated later if required
+             popupPanelHeaderCSS: "w3-teal",
+             popupPanelName: "" //expect this to come from a menu name working in popup mode
+        }
+        */
+
+        var effectiveOptions = {};
+        $.extend(effectiveOptions, _defPopupOptions);
+        if (typeof options === "object") {
+            $.extend(effectiveOptions, options);
+        } else if (typeof options === "string") {
+            effectiveOptions.popupPanelName = options;
+        }
+
+        //build a hidden div ready to accept content
+        if (!effectiveOptions.popupPanelID) effectiveOptions.popupPanelID = "dynamicPanel" + _dynamicDivPanelCounter++;
+
+
+        //build content for display  from the template
+        var sTemplate = _templatePopup;
+        //replace tokens
+        sTemplate = sTemplate.replace(/\[\[popupPanelID\]\]/g, effectiveOptions.popupPanelID);
+        sTemplate = sTemplate.replace(/\[\[popupPanelHeaderCSS\]\]/g, effectiveOptions.popupPanelHeaderCSS);
+        sTemplate = sTemplate.replace(/\[\[popupPanelName\]\]/g, effectiveOptions.popupPanelName);
+        debugger
+        var $content = $(sTemplate);
+        $("body").append($content);
+        //load the content from the url
+        $.ajax(url, {
+            success: function (data, textStatus, jqXHR) {
+                //stuff the text into the generated modal div
+                $("#" + effectiveOptions.popupPanelID + "_content").get(0).innerHTML = data;
+
+                //display the popup
+                navControl.modal(effectiveOptions.popupPanelID);
+            }
+        });
+        
 
     };
 
@@ -233,7 +272,7 @@ jQuery(window).ready(function () {
     //load the Nav into an element with ID "topNav"
     navControl.buildMenu("data/nav.xml", 1, function (oMenuRoot) {
         window.oMenuRoot = oMenuRoot;
-        ko.applyBindings(oMenuRoot, document.getElementById("topNav"))
+        ko.applyBindings(oMenuRoot, document.getElementById("topNav"));
     }, null);
 
 });
